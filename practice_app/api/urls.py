e9e65cb79bde472c87349d89553f383ffee1d8f9
Marcos <marcintosh87@gmail.com>
2022-05-03 14:25:16 -0400

    getting started

---
+++ practice_app/api/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from practice_app.api.views import movie_list, movie_detail
+
+urlpatterns = [
+   
+    path('list/', movie_list, name="movie_list"),
+    path('<int:pk>', movie_detail, name='movie_detail')
+]
---
+++ practice_app/api/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from practice_app.models import Movie
+from practice_app.api.serializer import MovieSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def movie_detail(request, pk):
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    if request.method == 'PATCH':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
